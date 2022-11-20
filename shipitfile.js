88d4cdd948318414d3ea3f188f74673d79510e09
Ralitsa Lefterova <ralitsa.lefterova@gmail.com>
2022-11-20 12:50:18 +0100

    test setup

--- shipitfile.js
+++ shipitfile.js
@@ -1,6 +1,7 @@
 module.exports = shipit => {
   // Load shipit-deploy tasks
-  require('shipit-deploy')(shipit)
+  require('shipit-deploy')(shipit);
+  require('shipit-shared')(shipit);
 
   shipit.initConfig({
     default: {
@@ -8,13 +9,47 @@ module.exports = shipit => {
       deployTo: '/root/Yoga-Style-eShop-backend',
       repositoryUrl: 'git@github.com:RalitsaLefterova/Yoga-Style-eShop-backend.git',
       keepReleases: 5,
+      shallowClone: true,
       shared: {
         overwrite: true,
         dirs: ['node_modules']
       }
     },
-    staging: {
-      servers: 'root@172.104.251.14',
+    production: {
+      servers: 'root@172.104.251.14'
     }
+  });
+
+  shipit.blTask('npm:install', async () => {
+    await shipit.remote(`cd ${shipit.releasePath} && npm install`);
   })
+
+  shipit.on('init', function () {
+    shipit.log('---------------1------------------');
+  });
+
+  shipit.on('fetched', function () {
+    shipit.log('---------------2------------------');
+  });
+
+  shipit.on('updated', function () {
+    shipit.log('---------------3------------------');
+  });
+
+  shipit.on('published', function () {
+    shipit.log('---------------4------------------');
+  });
+
+  shipit.on('cleaned', function () {
+    shipit.log('---------------5------------------');
+  });
+
+  shipit.on('finish', function () {
+    shipit.log('---------------6------------------');
+  });
+
+
+
+
+
 }
