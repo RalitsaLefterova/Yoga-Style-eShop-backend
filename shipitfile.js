730cb415ef892d3185a5e595b93ae459535becee
Ralitsa Lefterova <ralitsa.lefterova@gmail.com>
2023-09-24 16:44:41 +0200

    add production env

--- shipitfile.js
+++ shipitfile.js
@@ -4,72 +4,78 @@ module.exports = shipit => {
   require('shipit-shared')(shipit)
   const fs = require('fs')
 
-  const appName = 'api-yoga-style';
+  const appName = 'api-yoga-style'
+
+  const commonConfig = {
+    repositoryUrl: 'git@github.com:RalitsaLefterova/Yoga-Style-eShop-backend.git',
+    keepReleases: 5
+  }
 
   shipit.initConfig({
-    default: {
-      branch: 'main',
-      deployTo: '/root/Yoga-Style-eShop-backend',
-      repositoryUrl: 'git@github.com:RalitsaLefterova/Yoga-Style-eShop-backend.git',
-      keepReleases: 5,
-      // shared: {
-      //   overwrite: true,
-      //   dirs: ['node_modules']
-      // }
-      shared: {
-        dirs: [
-          {
-            path: 'node_modules',
-            overwrite: true,
-            chmod: '-R 777',
-          },
-          {
-            path: 'uploads/collections',
-            overwrite: true,
-            chmod: '-R 777',
-          },
-          {
-            path: 'uploads/products',
-            overwrite: true,
-            chmod: '-R 777',
-          }
-        ]
-      }
+    shared: {
+      dirs: [
+        {
+          path: 'node_modules',
+          overwrite: true,
+          chmod: '-R 777',
+        },
+        {
+          path: 'uploads/collections',
+          overwrite: true,
+          chmod: '-R 777',
+        },
+        {
+          path: 'uploads/products',
+          overwrite: true,
+          chmod: '-R 777',
+        }
+      ]
     },
-    dev: {
-      branch: 'development',
-      servers: 'root@172.104.251.14'
-    },
+    // development: {
+    //   ...commonConfig,
+    //   branch: 'development',
+    //   servers: 'root@172.104.251.14',
+    //   deployTo: '/root/Yoga-Style-eShop-backend-dev'
+    // },
     staging: {
-      servers: 'root@172.104.251.14'
+      ...commonConfig,
+      branch: 'main',
+      servers: 'root@172.104.251.14',
+      deployTo: '/root/Yoga-Style-eShop-backend-staging'
+    },
+    production: {
+      ...commonConfig,
+      branch: 'main',
+      servers: 'root@172.104.251.14',
+      deployTo: '/root/Yoga-Style-eShop-backend'
     }
-  });
+  })
 
   const path = require('path');
   const ecosystemFilePath = path.join(
     shipit.config.deployTo,
     'shared',
     'ecosystem.config.js'
-  );
+  )
   const uploadsFolderPath = path.join(
     shipit.config.deployTo,
     'shared',
     'uploads'
-  );
+  )
 
   shipit.blTask('npm:install', async () => {
-    await shipit.remote(`cd ${shipit.releasePath} && nvm use && npm install`);
+    await shipit.remote(`cd ${shipit.releasePath} && nvm use && npm install`)
   })
 
   shipit.blTask('copy-config', async () => {
     shipit.log('copying ecosystem.config.js file :: >>>>> ')
-    // await shipit.local(`scp /mnt/d/Projects-wip/Yoga-Style-eShop-backend/ecosystem.config.js root@172.104.251.14:/${shipit.releasePath}`);
-    await shipit.copyToRemote('/mnt/d/Projects-wip/Yoga-Style-eShop-backend/ecosystem.config.js', ecosystemFilePath);
+    // await shipit.local(`scp /mnt/d/Projects-wip/Yoga-Style-eShop-backend/ecosystem.config.js root@172.104.251.14:/${shipit.releasePath}`)
+    await shipit.copyToRemote('/mnt/d/Projects-wip/Yoga-Style-eShop-backend/ecosystem.config.js', ecosystemFilePath)
   })
 
   shipit.blTask('pm2-server', async () => {
     await shipit.remote(`pm2 delete -s ${appName} || :`);
-    await shipit.remote(`pm2 start ${shipit.currentPath}/src/index.js -n ${appName} ${ecosystemFilePath} --env production`);
+    await shipit.remote(`pm2 start ${shipit.currentPath}/src/index.js -n ${appName} ${ecosystemFilePath} --env production`)
   })
 
   shipit.blTask('create-symlink', async () => {
@@ -94,31 +100,31 @@ module.exports = shipit => {
   })
 
   // shipit.blTask('server:restart', async () => {
-  //   const command = 'pm2 restart all';
-  //   await shipit.remote(`cd ${shipit.config.deployTo} && ${command}`);
+  //   const command = 'pm2 restart all'
+  //   await shipit.remote(`cd ${shipit.config.deployTo} && ${command}`)
   // })
 
   shipit.on('init', function () {
-    shipit.log('--------------- 1 Init------------------');
-  });
+    shipit.log('--------------- 1 Init------------------')
+  })
 
   shipit.on('fetched', function () {
-    shipit.log('--------------- 2 Fetched ------------------');
-  });
+    shipit.log('--------------- 2 Fetched ------------------')
+  })
 
   shipit.on('updated', function () {
-    shipit.log('--------------- 3 Updated ------------------');
+    shipit.log('--------------- 3 Updated ------------------')
     shipit.start('npm:install', 'copy-config');
     shipit.start('create-symlink');
-  });
+  })
 
   shipit.on('published', function () {
-    shipit.log('--------------- 4 Published ------------------');
-  });
+    shipit.log('--------------- 4 Published ------------------')
+  })
 
   shipit.on('cleaned', function () {
-    shipit.log('--------------- 5 Cleaned ------------------');
+    shipit.log('--------------- 5 Cleaned ------------------')
     shipit.start('pm2-server');
-  });
+  })
 
 }
